e69d455c87985460ed60edc360b0274318d75a7e
Rogerndaba <rogerndaba@gmail.com>
2019-01-10 07:42:36 +0200

    fixed comment problem

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
 import sys
 import os.path
+import re
 
-class Handler:
-	def __init__(self):
-		self.name = 'roger'
-		self.age = 20
+# class Handler:
+# 	def __init__(self):
+# 		self.name = 'roger'
+# 		self.age = 20
 
-	def Start(self, ar):
-		print (ar + ' hello ' + self.name)
+# 	def Start(self, ar):
+# 		print (ar + ' hello ' + self.name)
 
 
 if len(sys.argv) == 2:
 	if os.path.exists(sys.argv[1]) == True:
 		f = open(sys.argv[1], "r")
 		col = []
-		i = 0
 		for line in f:
-			# print(line)
 			if line[0] == '#':
 				continue
 			x = line.split()
-			x2 = x[:x.index("#")] if "#" in x else x
-			if x2:
-				col.append(x2)
+			l = [x.index(i) for i in x if '#' in i]
+			col.append(x[:l[0]]) if l else col.append(x)
+
+		############# printing ##################
+		
 		for vals in col:
-			print(vals)
+			row_format = '{:<6}' * (len(vals))
+			print (row_format.format(*vals))
+
+		############# printing end ##############
 	else:
 		print('\033[91mno such file\033[0m')
 else:
 	print('\033[91minvalid number of arguments\033[0m')
 
 #
-human = Handler()
+# human = Handler()
 # human.Start('you')
