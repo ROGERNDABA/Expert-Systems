14bb719c2869bf4666f668bded8933c303b57678
rogerndaba <rogerndaba@gmail.com>
2019-08-11 18:37:10 +0200

    progress

--- main.py
+++ main.py
@@ -9,40 +9,66 @@ from pprint import pprint
 
 FactHandler = handler.FactHandler()
 
+def replace_all(text, dic):
+	for i, j in dic.items():
+		text = text.replace(i, str(j))
+		text = text.replace("Falsealse", "False")
+		text = text.replace("Truerue", "True")
+	return text
+
 if len(sys.argv) == 2:
-    if os.path.exists(sys.argv[1]) == True:
-        f = open(sys.argv[1], "r")
-        validFileLines = []
-        validFacts = {}
-        for line in f:
-            if line[0] == "#":
-                continue
-            x = line.split()
-            l = [x.index(i) for i in x if "#" in i]
-            validFileLines.append(x[: l[0]]) if l else validFileLines.append(x)
-        validFacts = FactHandler.validateAvailableOptions(validFileLines)
-        validFileLines = ["".join(listItem) for listItem in validFileLines if listItem]
+	if os.path.exists(sys.argv[1]) == True:
+		f = open(sys.argv[1], "r")
+		validFileLines = []
+		validFacts = {}
+		for line in f:
+			if line[0] == "#":
+				continue
+			x = line.split()
+			l = [x.index(i) for i in x if "#" in i]
+			validFileLines.append(x[: l[0]]) if l else validFileLines.append(x)
+		validFacts = FactHandler.validateAvailableOptions(validFileLines)
+		validFileLines = [" ".join(listItem) for listItem in validFileLines if listItem]
 
-        # print(validFileLines[-1])
-        validFileLines = [
-            re.findall(r"\([^()]*\)|<?=>|[-+/*|^]|\w+", listItem)
-            for listItem in validFileLines[:-2]
-        ]
-        # print()
-        # print(json.dumps(validFacts, indent=2))
-        # print(eval("True or True"))
-        ############# printing ##################
+		print(validFileLines)
+		validFileLines = validFileLines[:-2]
+		print(json.dumps(validFacts, indent=2))
+		lexer = ""
+		restart = True
 
-        # for vals in col1:
-        # 	row_format = '{:<6}' * (len(vals))
-        # 	print (row_format.format(*vals))
+		operands = {"+" : "and",
+					"!" : "not ",
+					"|" : "or",
+					"^" : "!="}
+		while restart:
+			for part in validFileLines:
+				# if re.match(r"[A-Z]\W[+|^]\W[A-Z]\W=>\W[A-Z]$", part):
+				part1 = replace_all(part, validFacts)
+				part1 = replace_all(part1, operands)
+				part1 = part1.split(" => ")
+				print(eval(part1[0]))
+			restart = False
+		print(lexer)
+		validFileLines = [
+			re.findall(r"\([^()]*\)|<?=>|[-+/*|^]|\w+", listItem)
+			for listItem in validFileLines[:-2]
+		]
 
-        ############# printing end ##############
+		# print()
+		# print(json.dumps(validFileLines, indent=2))
+		print(eval("( not True and True or False )"))
+		############# printing ##################
 
-    else:
-        print("\033[91mno such file or directory\033[0m")
+		# for vals in col1:
+		# 	row_format = '{:<6}' * (len(vals))
+		# 	print (row_format.format(*vals))
+
+		############# printing end ##############
+
+	else:
+		print("\033[91mno such file or directory\033[0m")
 else:
-    print("\033[91minvalid number of arguments\033[0m")
+	print("\033[91minvalid number of arguments\033[0m")
 
 # reserved for next attempt
 # ^[A-Z]*(=>|<=>)[A-Z]*$
